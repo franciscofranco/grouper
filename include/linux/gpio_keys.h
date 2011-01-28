6a17252e8158a7dba2243e50c9213d073b708466
Nitin Kumbhar <nkumbhar@nvidia.com>
2011-01-28 14:43:53 +0530

    input: gpio-keys: report wakeup key on resume
    During resume, gpio-keys does not get updated state of gpios which
    fails to report wakeup key event. Update resume routine to report
    key event based on wakeup source.
    
    BUG 745149
    
    Original-Change-Id: Iff7a9b431f9da8d226c8d3cd522725c3049fef07
    Reviewed-on: http://git-master/r/17455
    Tested-by: Nitin Kumbhar <nkumbhar@nvidia.com>
    Reviewed-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    
    Rebase-Id: Rde589cdb4f74683e9ea46933c7f949da16c5f884

--- include/linux/gpio_keys.h
+++ include/linux/gpio_keys.h
@@ -23,6 +23,7 @@ struct gpio_keys_platform_data {
 	int (*enable)(struct device *dev);
 	void (*disable)(struct device *dev);
 	const char *name;		/* input device name */
+	int (*wakeup_key)(void);
 };
 
 #endif
