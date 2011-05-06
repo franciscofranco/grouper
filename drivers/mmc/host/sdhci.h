285af9765fa89abd47c7820ca5ce415efeb9329e
Colin Cross <ccross@android.com>
2011-05-05 21:47:55 -0700

    mmc: host: sdhci-pltfm: Add suspend and resume hooks to sdhci platform
    Change-Id: I062fbe91d8af14366a92af1a27e9dab49869905c
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -274,6 +274,8 @@ struct sdhci_ops {
 	void	(*platform_reset_exit)(struct sdhci_host *host, u8 mask);
 	int	(*set_uhs_signaling)(struct sdhci_host *host, unsigned int uhs);
 
+	int		(*suspend)(struct sdhci_host *host, pm_message_t state);
+	int		(*resume)(struct sdhci_host *host);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
