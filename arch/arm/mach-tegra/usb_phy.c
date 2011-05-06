9cd78f9aa821b90b9e298d2abf897ee7ff01f54e
Rakesh Bodla <rbodla@nvidia.com>
2011-05-06 18:54:45 +0530

    ARM: tegra: usb_phy: Fix USB compilation warnings
    Removing unused variable.
    
    Original-Change-Id: Idf5fd40c7a953505c08872023b3d55296237ec49
    Reviewed-on: http://git-master/r/30690
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>
    
    Rebase-Id: R66a625864d32ffe745cd9c6399a3ca9fa5ad8f4b

--- arch/arm/mach-tegra/usb_phy.c
+++ arch/arm/mach-tegra/usb_phy.c
@@ -1793,7 +1793,6 @@ int tegra_usb_phy_bus_connect(struct tegra_usb_phy *phy)
 {
 	unsigned long val;
 	void __iomem *base = phy->regs;
-	struct tegra_ulpi_config *config = phy->config;
 
 	if (phy->usb_phy_type == TEGRA_USB_PHY_TYPE_HSIC) {
 #ifdef CONFIG_ARCH_TEGRA_3x_SOC
