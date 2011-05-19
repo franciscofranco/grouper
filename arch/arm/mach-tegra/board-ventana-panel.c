bcc0d8e2952f3a9601a05ff19d0c8af6d29b95fd
Bharat Nihalani <bnihalani@nvidia.com>
2011-05-19 20:09:14 +0530

    ARM: tegra: common: dynamic cpufreq governor
    To improve the power consumption situation for MP3 playback
    the scaling governor is set to conservative when display
    is turned off and the default governor is saved. The governor
    is restored when display is turned on.
    
    Bug 817727
    
    Original work done by "Wen Yi <wyi@nvidia.com>"
    
    Original-Change-Id: I43ffb0d508cc6d0a80eeeffcbab77526b644c437
    Reviewed-on: http://git-master/r/32194
    Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
    Tested-by: Niket Sirsi <nsirsi@nvidia.com>
    
    Rebase-Id: R341d5847eb472f8028985026985daac205973eb5

--- arch/arm/mach-tegra/board-ventana-panel.c
+++ arch/arm/mach-tegra/board-ventana-panel.c
@@ -350,7 +350,7 @@ static void ventana_panel_early_suspend(struct early_suspend *h)
 {
 	if (num_registered_fb > 0)
 		fb_blank(registered_fb[0], FB_BLANK_POWERDOWN);
-#ifdef CONFIG_CPU_FREQ
+#ifdef CONFIG_TEGRA_CONVSERVATIVE_GOV_ON_EARLYSUPSEND
 	cpufreq_save_default_governor();
 	cpufreq_set_conservative_governor();
 #endif
@@ -360,11 +360,9 @@ static void ventana_panel_late_resume(struct early_suspend *h)
 {
 	if (num_registered_fb > 0)
 		fb_blank(registered_fb[0], FB_BLANK_UNBLANK);
-
-#ifdef CONFIG_CPU_FREQ
+#ifdef CONFIG_TEGRA_CONVSERVATIVE_GOV_ON_EARLYSUPSEND
 	cpufreq_restore_default_governor();
 #endif
-
 }
 #endif
 
