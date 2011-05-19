6f4a299341a041e1806b9f5f8c7c0b7f1cf79b05
Terje Bergstrom <tbergstrom@nvidia.com>
2011-05-19 09:35:16 +0300

    nvhost: Make 3D workaround Tegra3 A01 only
    3D hardware workaround is needed for Tegra3 A01 only. With this patch, we
    read run-time whether it should be enabled or not.
    
    Workaround should be removed once A01's have been phased out.
    
    Bug 786316
    
    Original-Change-Id: Icd1b85b30a53c74d2e5c7a6df65a805d1fe5147c
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/32136
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    
    Rebase-Id: R6053c07b62aea3c2710d0d2ad6b5647a9fcf486b

---
+++ arch/arm/mach-tegra/include/mach/hardware.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2010 Google, Inc.
+ * Copyright (C) 2011 NVIDIA Corp.
+ *
+ * Author:
+ *	Colin Cross <ccross@google.com>
+ *	Erik Gilling <konkers@google.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef MACH_TEGRA_HARDWARE_H
+#define MACH_TEGRA_HARDWARE_H
+
+enum tegra_chipid {
+	TEGRA_CHIPID_UNKNOWN = 0,
+	TEGRA_CHIPID_TEGRA2 = 0x20,
+	TEGRA_CHIPID_TEGRA3 = 0x30,
+};
+
+enum tegra_revision {
+	TEGRA_REVISION_UNKNOWN = 0,
+	TEGRA_REVISION_A01,
+	TEGRA_REVISION_A02,
+#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
+	TEGRA_REVISION_A03,
+	TEGRA_REVISION_A03p,
+#endif
+	TEGRA_REVISION_MAX,
+};
+
+enum tegra_chipid tegra_get_chipid(void);
+enum tegra_revision tegra_get_revision(void);
+
+#endif
