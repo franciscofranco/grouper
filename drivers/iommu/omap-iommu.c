ebbd1eb505f1f7f9bea8da510e7fb8a7a63f22ce
Joerg Roedel <joerg.roedel@amd.com>
2011-09-06 17:56:07 +0200

    iommu/omap: Use bus_set_iommu instead of register_iommu
    Convert the OMAP IOMMU driver on ARM to use the new
    interface for publishing the iommu_ops.
    
    Cc: Ohad Ben-Cohen <ohad@wizery.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- drivers/iommu/omap-iommu.c
+++ drivers/iommu/omap-iommu.c
@@ -1225,7 +1225,7 @@ static int __init omap_iommu_init(void)
 		return -ENOMEM;
 	iopte_cachep = p;
 
-	register_iommu(&omap_iommu_ops);
+	bus_set_iommu(&platform_bus_type, &omap_iommu_ops);
 
 	return platform_driver_register(&omap_iommu_driver);
 }
