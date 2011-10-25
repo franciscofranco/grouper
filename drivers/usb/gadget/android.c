4652645a65d29a83fa129f7b92f7f0bb518a1559
Dan Willemsen <dwillemsen@nvidia.com>
2011-10-25 14:26:25 -0700

    usb: gadget: android: Update for 3.1
    Signed-off-by: Dan Willemsen <dwillemsen@nvidia.com>
    
    Rebase-Id: R57e373426d6e9f3a0be2610a0a74a565cc5ef9b6

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1035,6 +1035,7 @@ static struct usb_composite_driver android_usb_driver = {
 	.dev		= &device_desc,
 	.strings	= dev_strings,
 	.unbind		= android_usb_unbind,
+	.max_speed	= USB_SPEED_HIGH,
 };
 
 static int
