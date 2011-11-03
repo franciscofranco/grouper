8491d6d3a967ab0070891e5a68efd90aa36d9765
Sumit Bhattacharya <sumitb@nvidia.com>
2011-11-03 12:44:55 +0530

    ASoC: Tegra WM8903 machine: Add support for Tegra30 BT SCO
    Use BT SCO DAI-LINK interface for both Tegra20 and Tegra30. For Tegra30
    use i2s3 for BT SCO connection.
    
    Bug 872652
    Signed-off-by: Sumit Bhattacharya <sumitb@nvidia.com>
    
    Change-Id: I93d90843133267c1aed3386de5e7d40938952f53
    Reviewed-on: http://git-master/r/62051
    Tested-by: Sumit Bhattacharya <sumitb@nvidia.com>
    Reviewed-by: Scott Peterson <speterson@nvidia.com>
    
    Rebase-Id: R9d90a88375ecd8bad6f4c39a1ce083b87751b846

--- sound/soc/tegra/tegra_wm8903.c
+++ sound/soc/tegra/tegra_wm8903.c
@@ -617,7 +617,6 @@ static struct snd_soc_dai_link tegra_wm8903_dai[] = {
 		.codec_dai_name = "dit-hifi",
 		.ops = &tegra_spdif_ops,
 	},
-#ifdef CONFIG_ARCH_TEGRA_2x_SOC
 	{
 		.name = "BT-SCO",
 		.stream_name = "BT SCO PCM",
@@ -627,7 +626,6 @@ static struct snd_soc_dai_link tegra_wm8903_dai[] = {
 		.codec_dai_name = "dit-hifi",
 		.ops = &tegra_wm8903_bt_sco_ops,
 	},
-#endif
 };
 
 static struct snd_soc_card snd_soc_tegra_wm8903 = {
@@ -678,6 +676,8 @@ static __devinit int tegra_wm8903_driver_probe(struct platform_device *pdev)
 		tegra_wm8903_dai[0].cpu_dai_name = "tegra30-i2s.1";
 
 		tegra_wm8903_dai[1].cpu_dai_name = "tegra30-spdif";
+
+		tegra_wm8903_dai[2].cpu_dai_name = "tegra30-i2s.3";
 	}
 
 	card->dev = &pdev->dev;
