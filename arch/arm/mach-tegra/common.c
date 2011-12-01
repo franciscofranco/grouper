GalaxyTab101/samsung-kernel-galaxytab101

arm: tegra: Change Tegra3 L2 cache prefetch to next line
Change L2 cache prefetch offset from 8th line to next line.

Change-Id: Ie88008e2ab5a882235ae91d71d193e898ca67121
Reviewed-on: http://git-master/r/33195
Reviewed-by: Niket Sirsi <nsirsi@nvidia.com>
Tested-by: Niket Sirsi <nsirsi@nvidia.com>

arch/arm/mach-tegra/common.c
@@ -161,7 +161,7 @@ void __init tegra_init_cache(void)
 #endif
 
 	/* Enable PL310 double line fill feature. */
-	writel(((1<<30) | 7), p + L2X0_PREFETCH_OFFSET);
+	writel(((1<<30) | 0), p + L2X0_PREFETCH_OFFSET);
 #endif
 	aux_ctrl = readl(p + L2X0_CACHE_TYPE);
 	aux_ctrl = (aux_ctrl & 0x700) << (17-8);

franciscofranco/grouper

Original-Change-Id: Ie88008e2ab5a882235ae91d71d193e898ca67121
Rebase-Id: R3826be5d5531d275a624193f3063372552b05743

arch/arm/mach-tegra/common.c
@@ -150,7 +150,7 @@ void tegra_init_cache(void)
 #endif
 
 	/* Enable PL310 double line fill feature. */
-	writel(((1<<30) | 7), p + L2X0_PREFETCH_CTRL);
+	writel(((1<<30) | 0), p + L2X0_PREFETCH_CTRL);
 #endif
 	aux_ctrl = readl(p + L2X0_CACHE_TYPE);
 	aux_ctrl = (aux_ctrl & 0x700) << (17-8);