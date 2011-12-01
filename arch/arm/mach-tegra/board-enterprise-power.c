fosser2/android-tegra-nv-3.1.10-rel-15r7

arm: tegra: enterprise: Enable battery charging based on kernel option
If kernel command line option have the power_supply as battery
then enable the battery charging functionality.

Change-Id: I0f7c5e03b0505fbd4161434f09d0084d33e3397f
Reviewed-on: http://git-master/r/50673
Reviewed-by: Thomas Cherry <tcherry@nvidia.com>
Tested-by: Thomas Cherry <tcherry@nvidia.com>
Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>

arch/arm/mach-tegra/board-enterprise-power.c
@@ -460,7 +460,6 @@ int __init enterprise_regulator_init(void)
 {
 	void __iomem *pmc = IO_ADDRESS(TEGRA_PMC_BASE);
 	u32 pmc_ctrl;
-	struct board_info board_info;
 
 	/* configure the power management controller to trigger PMU
 	 * interrupts when low */
@@ -468,11 +467,8 @@ int __init enterprise_regulator_init(void)
 	pmc_ctrl = readl(pmc + PMC_CTRL);
 	writel(pmc_ctrl | PMC_CTRL_INTR_LOW, pmc + PMC_CTRL);
 
-	tegra_get_board_info(&board_info);
-
-	/* Disable battery charging for board whose sku does not
-	   have battery support */
-	if (!(board_info.sku & SKU_BATTERY_SUPPORT)) {
+	/* Disable battery charging if power adapter is connected. */
+	if (get_power_supply_type() == power_supply_adapter) {
 		bcharger_pdata.num_consumer_supplies = 0;
 		bcharger_pdata.consumer_supplies = NULL;
 	}

franciscofranco/grouper

(cherry picked from commit 4f36edabc585f3fea28e2f959558ac7e07dd2b5e)
Change-Id: I5a12105503f29e4e58991bcf4c020c709a97a7cd
Reviewed-on: http://git-master/r/56994
Reviewed-by: Syed Rafiuddin <srafiuddin@nvidia.com>
Tested-by: Syed Rafiuddin <srafiuddin@nvidia.com>
Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
Rebase-Id: R9fa09e3c265fabefd14804def31100259bfd9730

arch/arm/mach-tegra/board-enterprise-power.c
@@ -473,7 +473,6 @@ int __init enterprise_regulator_init(void)
 {
 	void __iomem *pmc = IO_ADDRESS(TEGRA_PMC_BASE);
 	u32 pmc_ctrl;
-	struct board_info board_info;
 
 	/* configure the power management controller to trigger PMU
 	 * interrupts when low */
@@ -481,11 +480,8 @@ int __init enterprise_regulator_init(void)
 	pmc_ctrl = readl(pmc + PMC_CTRL);
 	writel(pmc_ctrl | PMC_CTRL_INTR_LOW, pmc + PMC_CTRL);
 
-	tegra_get_board_info(&board_info);
-
-	/* Disable battery charging for board whose sku does not
-	   have battery support */
-	if (!(board_info.sku & SKU_BATTERY_SUPPORT)) {
+	/* Disable battery charging if power adapter is connected. */
+	if (get_power_supply_type() == POWER_SUPPLY_TYPE_MAINS) {
 		bcharger_pdata.num_consumer_supplies = 0;
 		bcharger_pdata.consumer_supplies = NULL;
 	}