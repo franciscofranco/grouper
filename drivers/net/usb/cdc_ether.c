fosser2/android-tegra-nv-3.1.10-rel-15r7

cdc_ether: Force Tango cdc-ether to enumerate as wwan0
bug 860095
Change-Id: I889a990a2079b5e668c5668ac0a8af6aa0afd95a
Reviewed-on: http://git-master/r/45552
Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

drivers/net/usb/cdc_ether.c
@@ -596,17 +596,24 @@ static const struct usb_device_id	products [] = {
 
 /* PH450 */
 {
-         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
-                | USB_DEVICE_ID_MATCH_DEVICE,
-         USB_DEVICE(0x1983,0x0310),
-         .driver_info = (unsigned long)&mbm_info,
+	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+		 | USB_DEVICE_ID_MATCH_DEVICE,
+	USB_DEVICE(0x1983,0x0310),
+	.driver_info = (unsigned long)&mbm_info,
 }, {
-         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
-                | USB_DEVICE_ID_MATCH_DEVICE,
-         USB_DEVICE(0x1983,0x0321),
-         .driver_info = (unsigned long)&mbm_info,
+	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+		 | USB_DEVICE_ID_MATCH_DEVICE,
+	USB_DEVICE(0x1983,0x0321),
+	.driver_info = (unsigned long)&mbm_info,
 },
 
+/* Tango module */
+{
+	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+		 | USB_DEVICE_ID_MATCH_DEVICE,
+	USB_DEVICE(0x0489,0xE03A),
+	.driver_info = (unsigned long)&mbm_info,
+},
 /*
  * WHITELIST!!!
  *

franciscofranco/grouper

Original-Change-Id: I889a990a2079b5e668c5668ac0a8af6aa0afd95a
Rebase-Id: R6ab06a2d2a8474fa4efef7e666508ba1d06d3fe6

drivers/net/usb/cdc_ether.c
@@ -572,17 +572,24 @@ static const struct usb_device_id	products [] = {
 
 /* PH450 */
 {
-         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
-                | USB_DEVICE_ID_MATCH_DEVICE,
-         USB_DEVICE(0x1983,0x0310),
-         .driver_info = (unsigned long)&wwan_info,
+	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+		| USB_DEVICE_ID_MATCH_DEVICE,
+	USB_DEVICE(0x1983,0x0310),
+	.driver_info = (unsigned long)&wwan_info,
 }, {
-         .match_flags = USB_DEVICE_ID_MATCH_INT_INFO
-                | USB_DEVICE_ID_MATCH_DEVICE,
-         USB_DEVICE(0x1983,0x0321),
-         .driver_info = (unsigned long)&wwan_info,
+	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+		| USB_DEVICE_ID_MATCH_DEVICE,
+	USB_DEVICE(0x1983,0x0321),
+	.driver_info = (unsigned long)&wwan_info,
 },
 
+/* Tango module */
+{
+	.match_flags = USB_DEVICE_ID_MATCH_INT_INFO
+		 | USB_DEVICE_ID_MATCH_DEVICE,
+	USB_DEVICE(0x0489,0xE03A),
+	.driver_info = (unsigned long)&wwan_info,
+},
 /*
  * WHITELIST!!!
  *