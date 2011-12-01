fosser2/android-tegra-nv-3.1.10-rel-15r7

ARM: tegra: gpio: Set a gpio to tristate or normal
Create mapping from gpio to pingroup and set gpio to
normal or tristate

Bug 866633

Change-Id: I4ca31c8048437385efdd8a9694f7bb89355f4aef
Reviewed-on: http://git-master/r/53184
Reviewed-by: Bandi Krishna Chaitanya <bandik@nvidia.com>
Tested-by: Bandi Krishna Chaitanya <bandik@nvidia.com>
Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

arch/arm/mach-tegra/pinmux.c
@@ -34,6 +34,8 @@
 
 static const struct tegra_pingroup_desc *const pingroups = tegra_soc_pingroups;
 static const struct tegra_drive_pingroup_desc *const drive_pingroups = tegra_soc_drive_pingroups;
+static const int *gpio_to_pingroups_map =  gpio_to_pingroup;
+
 
 static char *tegra_mux_names[TEGRA_MAX_MUX] = {
 	[TEGRA_MUX_AHB_CLK] = "AHB_CLK",
@@ -219,7 +221,7 @@ static const char *pupd_name(unsigned long val)
 
 static const char *lock_name(unsigned long val)
 {
-	switch(val) {
+	switch (val) {
 	case TEGRA_PIN_LOCK_DEFAULT:
 		return "LOCK_DEFUALT";
 
@@ -235,7 +237,7 @@ static const char *lock_name(unsigned long val)
 
 static const char *od_name(unsigned long val)
 {
-	switch(val) {
+	switch (val) {
 	case TEGRA_PIN_OD_DEFAULT:
 		return "OD_DEFAULT";
 
@@ -251,7 +253,7 @@ static const char *od_name(unsigned long val)
 
 static const char *ioreset_name(unsigned long val)
 {
-	switch(val) {
+	switch (val) {
 	case TEGRA_PIN_IO_RESET_DEFAULT:
 		return "IO_RESET_DEFAULT";
 
@@ -289,6 +291,11 @@ static inline unsigned long pg_readl(unsigned long offset)
 static inline void pg_writel(unsigned long value, unsigned long offset)
 {
 	writel(value, IO_TO_VIRT(TEGRA_APB_MISC_BASE) + offset);
+}
+
+int tegra_pinmux_get_pingroup(int gpio_nr)
+{
+	return gpio_to_pingroup[gpio_nr];
 }
 
 static int tegra_pinmux_set_func(const struct tegra_pingroup_config *config)

franciscofranco/grouper

Reviewed-on: http://git-master/r/56557
(cherry picked from commit 321ded98d41170b9e32d60177c6808492ccdf115)
Change-Id: I3d1b979717f1c6b208af3df0a7dfe603e5272d21
Reviewed-on: http://git-master/r/61120
Rebase-Id: R5991c2cbc11aa35345fde7f08c0bfeb306e85e1e

arch/arm/mach-tegra/pinmux.c
@@ -35,6 +35,8 @@
 
 static const struct tegra_pingroup_desc *const pingroups = tegra_soc_pingroups;
 static const struct tegra_drive_pingroup_desc *const drive_pingroups = tegra_soc_drive_pingroups;
+static const int *gpio_to_pingroups_map =  gpio_to_pingroup;
+
 
 static char *tegra_mux_names[TEGRA_MAX_MUX] = {
 	[TEGRA_MUX_AHB_CLK] = "AHB_CLK",
@@ -221,7 +223,7 @@ static const char *pupd_name(unsigned long val)
 #if !defined(CONFIG_ARCH_TEGRA_2x_SOC)
 static const char *lock_name(unsigned long val)
 {
-	switch(val) {
+	switch (val) {
 	case TEGRA_PIN_LOCK_DEFAULT:
 		return "LOCK_DEFUALT";
 
@@ -237,7 +239,7 @@ static const char *lock_name(unsigned long val)
 
 static const char *od_name(unsigned long val)
 {
-	switch(val) {
+	switch (val) {
 	case TEGRA_PIN_OD_DEFAULT:
 		return "OD_DEFAULT";
 
@@ -253,7 +255,7 @@ static const char *od_name(unsigned long val)
 
 static const char *ioreset_name(unsigned long val)
 {
-	switch(val) {
+	switch (val) {
 	case TEGRA_PIN_IO_RESET_DEFAULT:
 		return "IO_RESET_DEFAULT";
 
@@ -292,6 +294,11 @@ static inline unsigned long pg_readl(unsigned long offset)
 static inline void pg_writel(unsigned long value, unsigned long offset)
 {
 	writel(value, IO_TO_VIRT(TEGRA_APB_MISC_BASE) + offset);
+}
+
+int tegra_pinmux_get_pingroup(int gpio_nr)
+{
+	return gpio_to_pingroups_map[gpio_nr];
 }
 
 static int tegra_pinmux_set_func(const struct tegra_pingroup_config *config)