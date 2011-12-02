d5023aa5162f11dca7f8c8bf07cd868ea8fd7112
Prashant Gaikwad <pgaikwad@nvidia.com>
2011-12-02 16:24:15 +0530

    ARM: tegra: cpufreq: add cpu to emc mapping for AP25
    With current mapping for CPU frequency greater than 816 MHz EMC
    frequency will be always 600 MHz. AP25 max EMC frequency is
    760MHz. If CPU frequency is greater than 1GHz then set
    EMC frequency to 760 MHz.
    
    Change-Id: I617e1637bd14c3c8560ffdfa17769f6554059609
    Signed-off-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Reviewed-on: http://git-master/r/67893
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>

--- arch/arm/mach-tegra/tegra2_clocks.c
+++ arch/arm/mach-tegra/tegra2_clocks.c
@@ -2688,7 +2688,9 @@ struct tegra_cpufreq_table_data *tegra_cpufreq_table_get(void)
 unsigned long tegra_emc_to_cpu_ratio(unsigned long cpu_rate)
 {
 	/* Vote on memory bus frequency based on cpu frequency */
-	if (cpu_rate >= 816000)
+	if (cpu_rate > 1000000000)
+		return 760000000;
+	else if (cpu_rate >= 816000)
 		return 600000000;	/* cpu 816 MHz, emc max */
 	else if (cpu_rate >= 608000)
 		return 300000000;	/* cpu 608 MHz, emc 150Mhz */
