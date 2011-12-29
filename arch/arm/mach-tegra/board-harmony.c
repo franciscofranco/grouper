570c3438ea78f13fe9979d7a7022f063beecbad1
Preetham Chandru <pchandru@nvidia.com>
2011-12-29 18:55:34 +0530

    arm: tegra: Enable HDMI for harmony
    Enabled HDMI support for harmony.
    
    Bug: 911662
    
    Change-Id: I50d513dc0705fb64fc3946c1518b60b885d6194e
    Signed-off-by: Preetham Chandru R <pchandru@nvidia.com>
    Reviewed-on: http://git-master/r/72611
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/board-harmony.c
+++ arch/arm/mach-tegra/board-harmony.c
@@ -507,7 +507,7 @@ void __init tegra_harmony_reserve(void)
 	if (memblock_reserve(0x0, 4096) < 0)
 		pr_warn("Cannot reserve first 4K of memory for safety\n");
 
-	tegra_reserve(SZ_128M, SZ_8M, 0);
+	tegra_reserve(SZ_128M, SZ_8M, SZ_16M);
 }
 
 MACHINE_START(HARMONY, "harmony")
