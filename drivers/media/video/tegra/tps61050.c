fe3c458f1c4909b2ed361aff9a02766a9f97efbc
Colin Patrick McCabe <cmccabe@nvidia.com>
2012-01-19 15:15:39 -0800

    media: tps61050: fix NVC_IOCTL_PWR_RD
    Reviewed-on: http://git-master/r/77403
    
    Change-Id: If51967720de2f4dc1634906fa263c693aef87d75
    Signed-off-by: Colin Patrick McCabe <cmccabe@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/78012
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/media/video/tegra/tps61050.c
+++ drivers/media/video/tegra/tps61050.c
@@ -762,6 +762,7 @@ static long tps61050_ioctl(struct file *file,
 					__func__, __LINE__);
 			return -EFAULT;
 		}
+		return 0;
 
 	default:
 		dev_err(&info->i2c_client->dev, "%s unsupported ioctl: %x\n",
