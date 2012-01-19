afc9accc08f3bb2731a2f33c2edab9024f0cf58b
Colin Patrick McCabe <cmccabe@nvidia.com>
2012-01-19 14:04:10 -0800

    media: fix corner case in tegra_mediaserver
    Reviewed-on: http://git-master/r/77465
    
    Change-Id: I653afbb6f766493e5d458b2ba4babb8e400c30c1
    Signed-off-by: Colin Patrick McCabe <cmccabe@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/78016
    Reviewed-by: Automatic_Commit_Validation_User

--- drivers/media/video/tegra/mediaserver/tegra_mediaserver.c
+++ drivers/media/video/tegra/mediaserver/tegra_mediaserver.c
@@ -394,7 +394,7 @@ static int mediasrv_update_block_info(
 )
 {
 	struct tegra_mediasrv_block *entry = NULL;
-	struct tegra_mediasrv_block *block;
+	struct tegra_mediasrv_block *block = NULL;
 	int e;
 
 	list_for_each_entry(entry, &node->blocks, entry) {
