d755e3062055994689a2d5c202f1ba1b71c6a275
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-01-25 20:46:53 +0000

    regmap: Unexport regcache_write() and regcache_read()
    They have no current users which is fortunate as they don't take the lock
    and therefore aren't safe to use externally. We'll need to add new
    operations if direct cache access is needed.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    (cherry picked from commit 7e53b195e412a813e915843adc7e4d91868e8e94)
    
    Change-Id: I6a273d8f42fafe5f2d0f2269dd9e0253e8ac8e3f
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/87578

--- drivers/base/regmap/regcache.c
+++ drivers/base/regmap/regcache.c
@@ -210,7 +210,6 @@ int regcache_read(struct regmap *map,
 
 	return -EINVAL;
 }
-EXPORT_SYMBOL_GPL(regcache_read);
 
 /**
  * regcache_write: Set the value of a given register in the cache.
@@ -237,7 +236,6 @@ int regcache_write(struct regmap *map,
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(regcache_write);
 
 /**
  * regcache_sync: Sync the register cache with the hardware.
