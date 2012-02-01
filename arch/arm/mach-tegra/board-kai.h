a1a27202b7ebf3ebeb3119ba3c60aafb4c3ed0cd
Ali Ekici <aekici@nvidia.com>
2012-02-01 15:38:45 -0800

    arm: tegra: Raydium DirectTouch Board file
    Modified files supports unified DirectTouch that can
    work both for Cardhu and Kai. Touch configuration
    is passed to driver with these board files.
    
    Bug 832605
    
    Signed-off-by: Ali Ekici <aekici@nvidia.com>
    Change-Id: I075aaad2e71ee1a87b23680ef629fc99bc42ea93
    Reviewed-on: http://git-master/r/80129
    Reviewed-by: Lokesh Pathak <lpathak@nvidia.com>
    Tested-by: Lokesh Pathak <lpathak@nvidia.com>
    (cherry picked from commit dc1372f43159640207a0d4e0ff08dde5bafc1baa)
    Reviewed-on: http://git-master/r/83569
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/board-kai.h
+++ arch/arm/mach-tegra/board-kai.h
@@ -71,7 +71,7 @@ int kai_keys_init(void);
 int kai_pins_state_init(void);
 int kai_edp_init(void);
 void __init kai_tsensor_init(void);
-int __init touch_init_raydium(void);
+int __init touch_init_raydium(int irq_gpio, int reset_gpio, int platform);
 int __init touch_init_synaptics_kai(void);
 
 #define TOUCH_GPIO_IRQ_RAYDIUM_SPI      TEGRA_GPIO_PZ3
