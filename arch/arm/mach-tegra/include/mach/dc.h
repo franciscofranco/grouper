24ee99196e2b8a56f12b867b4e1e6e6c54b36e76
Kevin Huang <kevinh@nvidia.com>
2012-02-03 18:02:42 -0800

    video: tegra: dc: Schedule delayed work to clear emc bandwidth.
    Bug 932840
    
    Change-Id: I12d8d2d2cd42d0dafea38463ad77b44f7e64d7c1
    Signed-off-by: Kevin Huang <kevinh@nvidia.com>
    Reviewed-on: http://git-master/r/83645
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -537,6 +537,7 @@ struct tegra_dc_pwm_params {
 void tegra_dc_config_pwm(struct tegra_dc *dc, struct tegra_dc_pwm_params *cfg);
 
 int tegra_dsi_send_panel_short_cmd(struct tegra_dc *dc, u8 *pdata, u8 data_len);
+void tegra_dc_host_trigger(struct tegra_dc *dc);
 
 int tegra_dc_update_csc(struct tegra_dc *dc, int win_index);
 
