caa6566d4fb8539d09276c1bcb818444af675624
naveenk <naveenk@nvidia.com>
2012-02-14 12:21:15 +0530

    mmc: tegra: enabling MMC_PM_KEEP_POWER flag for built_in devices
    During resume mmc_power_up is not necessary for eMMC
    
    Bug 932606
    
    Change-Id: I6dd6e7c4f5582ecca8b9c459e3537b05cce69e69
    Signed-off-by: naveen kumar arepalli<naveenk@nvidia.com>
    Reviewed-on: http://git-master/r/83709
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -815,7 +815,7 @@ static int __devinit sdhci_tegra_probe(struct platform_device *pdev)
 	host->mmc->pm_caps |= MMC_PM_KEEP_POWER | MMC_PM_IGNORE_PM_NOTIFY;
 	if (plat->mmc_data.built_in) {
 		host->mmc->caps |= MMC_CAP_NONREMOVABLE;
-		host->mmc->pm_flags |= MMC_PM_IGNORE_PM_NOTIFY;
+		host->mmc->pm_flags |= MMC_PM_IGNORE_PM_NOTIFY | MMC_PM_KEEP_POWER;
 	}
 	/* Do not turn OFF embedded sdio cards as it support Wake on Wireless */
 	if (plat->mmc_data.embedded_sdio)
