1ffc0bc504fab736e4361f4850e686ae30f4c1a5
Sheshagiri Shenoy <sshenoy@nvidia.com>
2012-02-17 16:33:50 -0800

    arm: tegra: kai: enable modem regulator
    bug 937956
    
    Change-Id: I765f2fb07e438d91e4e3c338206f7d0899c3c67d
    Signed-off-by: Sheshagiri Shenoy <sshenoy@nvidia.com>
    Reviewed-on: http://git-master/r/84661
    Reviewed-by: Steve Lin <stlin@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- arch/arm/mach-tegra/board-kai-power.c
+++ arch/arm/mach-tegra/board-kai-power.c
@@ -462,7 +462,7 @@ FIXED_REG(3, en_1v8_cam_a00,	en_1v8_cam,		max77663_rails(sd2),
 FIXED_REG(4, en_vddio_vid_a00,	en_vddio_vid,		NULL,
 	0,	0,	TEGRA_GPIO_PB2,				true,	0,	5000);
 FIXED_REG(5, en_3v3_modem_a00,	en_3v3_modem,		NULL,
-	0,	0,	TEGRA_GPIO_PP0,				true,	0,	3300);
+	1,	1,	TEGRA_GPIO_PP0,				true,	0,	3300);
 FIXED_REG(6, en_vdd_pnl_a00,	en_vdd_pnl,		FIXED_SUPPLY(en_3v3_sys_a00),
 	0,	0,	TEGRA_GPIO_PW1,				true,	0,	3300);
 FIXED_REG(7, en_cam3_ldo_a00,	en_cam3_ldo,		FIXED_SUPPLY(en_3v3_sys_a00),
@@ -486,7 +486,7 @@ FIXED_REG(3, en_1v8_cam_a01,	en_1v8_cam,		max77663_rails(sd2),
 FIXED_REG(4, en_vddio_vid_a01,	en_vddio_vid,		NULL,
 	0,	0,	TEGRA_GPIO_PB2,				true,	0,	5000);
 FIXED_REG(5, en_3v3_modem_a01,	en_3v3_modem,		NULL,
-	0,	0,	TEGRA_GPIO_PP0,				true,	0,	3300);
+	1,	1,	TEGRA_GPIO_PP0,				true,	0,	3300);
 FIXED_REG(6, en_vdd_pnl_a01,	en_vdd_pnl,		FIXED_SUPPLY(en_3v3_sys_a01),
 	0,	0,	TEGRA_GPIO_PW1,				true,	0,	3300);
 FIXED_REG(7, en_cam3_ldo_a01,	en_cam3_ldo,		FIXED_SUPPLY(en_3v3_sys_a01),
