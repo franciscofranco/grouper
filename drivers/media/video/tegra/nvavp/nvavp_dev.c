5f945dc9792d30adf049216cfc5fbd1fb913a9f0
Hiroshi DOYU <hdoyu@nvidia.com>
2012-02-21 15:32:50 +0200

    media: video: tegra: nvavp: Fix build warning
    Use correct type.
    
    Change-Id: Ic08214f97d721311dde07ac35664bc3b766ba131
    Signed-off-by: Hiroshi DOYU <hdoyu@nvidia.com>
    Reviewed-on: http://git-master/r/84948
    Reviewed-by: Scott Williams <scwilliams@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- drivers/media/video/tegra/nvavp/nvavp_dev.c
+++ drivers/media/video/tegra/nvavp/nvavp_dev.c
@@ -814,7 +814,7 @@ static int nvavp_set_clock_ioctl(struct file *filp, unsigned int cmd,
 	if (copy_from_user(&config, (void __user *)arg, sizeof(struct nvavp_clock_args)))
 		return -EFAULT;
 
-	dev_dbg(&nvavp->nvhost_dev->dev, "%s: clk_id=%d, clk_rate=%lu\n",
+	dev_dbg(&nvavp->nvhost_dev->dev, "%s: clk_id=%d, clk_rate=%u\n",
 			__func__, config.id, config.rate);
 
 	if (config.id == NVAVP_MODULE_ID_AVP)
