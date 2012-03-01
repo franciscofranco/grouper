98baa8b0d02f2ed9aa2e93d3787cfd62cc323f0e
Diwakar Tundlam <dtundlam@nvidia.com>
2012-02-29 16:18:11 -0800

    ARM: tegra: power: Trim Tegra3 hotplug timings
    Bug 945921
    Reduce delays in going LP->G and G->Gn
    
    (cherry picked from commit 51b7022f328d955cc4dbbf47b35403ea9c5e3a7b)
    Signed-off-by: Diwakar Tundlam <dtundlam@nvidia.com>
    
    Change-Id: I10f68196cb64ea20461000e9b0d64999fd714395
    Reviewed-on: http://git-master/r/88169
    Reviewed-by: Diwakar Tundlam <dtundlam@nvidia.com>
    Tested-by: Diwakar Tundlam <dtundlam@nvidia.com>

--- arch/arm/mach-tegra/cpu-tegra3.c
+++ arch/arm/mach-tegra/cpu-tegra3.c
@@ -39,8 +39,8 @@
 #include "clock.h"
 
 #define INITIAL_STATE		TEGRA_HP_DISABLED
-#define UP2G0_DELAY_MS		200
-#define UP2Gn_DELAY_MS		1000
+#define UP2G0_DELAY_MS		70
+#define UP2Gn_DELAY_MS		100
 #define DOWN_DELAY_MS		2000
 
 static struct mutex *tegra3_cpu_lock;
