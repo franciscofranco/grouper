b79ffddd49f67bab70c857c53f46b7bd98bf7fa8
singhome_lee <singhome_lee@asus.com>
2012-03-09 15:57:34 +0800

    revert "scheduler: domain: init next_balance in nohz_idle_balancer with jiffies"
    [SR 941625] reboot stress test fail with 14r9
    
    Change-Id: I60a365591cbc93645a0a7df289ac99c7d64ec01a
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/45238
    Reviewed-by: Singhome Lee <singhome_lee@asus.com>
    Tested-by: Singhome Lee <singhome_lee@asus.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -8186,7 +8186,7 @@ void __init sched_init(void)
 	atomic_set(&nohz.load_balancer, nr_cpu_ids);
 	atomic_set(&nohz.first_pick_cpu, nr_cpu_ids);
 	atomic_set(&nohz.second_pick_cpu, nr_cpu_ids);
-	nohz.next_balance = jiffies;
+	//nohz.next_balance = jiffies;
 #endif
 	/* May be allocated at isolcpus cmdline parse time */
 	if (cpu_isolated_map == NULL)
