5debaf0f94487a8a12dcbd17c3244677caef0a97
Nitin Bindal <nbindal@nvidia.com>
2012-03-20 14:46:06 +0530

    arm: tegra: enterprise: enable out of band interrupt
    configuring wf_wakeup gpio
    
    Change-Id: I05e907c36847da07990d440357b9ef4ae3a857be
    Signed-off-by: Nitin Bindal <nbindal@nvidia.com>
    Reviewed-on: http://git-master/r/90994
    Reviewed-by: Narayan Reddy <narayanr@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board-enterprise-pinmux.c
+++ arch/arm/mach-tegra/board-enterprise-pinmux.c
@@ -257,7 +257,7 @@ static __initdata struct tegra_pingroup_config enterprise_pinmux[] = {
 	DEFAULT_PINMUX(GPIO_PU3,        UARTA,           NORMAL,    NORMAL,     OUTPUT),
 	DEFAULT_PINMUX(GPIO_PU4,        PWM1,            NORMAL,    NORMAL,     OUTPUT),
 	DEFAULT_PINMUX(GPIO_PU5,        PWM2,            NORMAL,    NORMAL,     INPUT),
-	DEFAULT_PINMUX(GPIO_PU6,        PWM3,            NORMAL,    NORMAL,     OUTPUT),
+	DEFAULT_PINMUX(GPIO_PU6,        PWM3,            NORMAL,    NORMAL,     INPUT),
 	DEFAULT_PINMUX(DAP4_FS,         I2S3,            NORMAL,    NORMAL,     INPUT),
 	DEFAULT_PINMUX(DAP4_DIN,        I2S3,            NORMAL,    NORMAL,     INPUT),
 	DEFAULT_PINMUX(DAP4_DOUT,       I2S3,            NORMAL,    NORMAL,     INPUT),
