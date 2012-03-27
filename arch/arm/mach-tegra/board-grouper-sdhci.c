b5b30bbba1dcf4f279a59af18535d4c1d6816493
andy2_kuo <andy2_kuo@asus.com>
2012-03-27 19:29:03 +0800

    arm: tegra: grouper: add support for wifi
    1) Enable TEGRA_GPIO_PD3, TEGRA_GPIO_PD4, TEGRA_GPIO_PO4 for WiFi.
    2) Add platform device grouper_wifi_device for WiFi.
    3) Set CONFIG_BCMDHD=y to built in WiFi driver.
    
    Change-Id: Ie5ad8416f5c35bbf17935f9b3b0357c29a964a00
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/45177
    Reviewed-by: Andy2 Kuo <andy2_kuo@asus.com>
    Tested-by: Andy2 Kuo <andy2_kuo@asus.com>
    Reviewed-by: Vincent Yue <Vincent_Yue@asus.com>

--- arch/arm/mach-tegra/board-grouper-sdhci.c
+++ arch/arm/mach-tegra/board-grouper-sdhci.c
@@ -17,6 +17,7 @@
 
 #include <linux/resource.h>
 #include <linux/platform_device.h>
+#include <linux/wlan_plat.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
 #include <linux/clk.h>
@@ -33,6 +34,42 @@
 #include "board-grouper.h"
 
 #define KAI_SD_CD	TEGRA_GPIO_PI5
+#define GROUPER_WLAN_PWR	TEGRA_GPIO_PD4
+#define GROUPER_WLAN_RST	TEGRA_GPIO_PD3
+#define GROUPER_WLAN_WOW	TEGRA_GPIO_PO4
+
+static void (*wifi_status_cb)(int card_present, void *dev_id);
+static void *wifi_status_cb_devid;
+static int grouper_wifi_status_register(void (*callback)(int , void *),void *);
+
+static int grouper_wifi_reset(int on);
+static int grouper_wifi_power(int on);
+static int grouper_wifi_set_carddetect(int val);
+
+static struct wifi_platform_data grouper_wifi_control = {
+	.set_power	= grouper_wifi_power,
+	.set_reset	= grouper_wifi_reset,
+	.set_carddetect	= grouper_wifi_set_carddetect,
+};
+
+static struct resource wifi_resource[] = {
+	[0] = {
+		.name	= "bcmdhd_wlan_irq",
+		.start	= TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PO4),
+		.end	= TEGRA_GPIO_TO_IRQ(TEGRA_GPIO_PO4),
+		.flags	= IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
+	},
+};
+
+static struct platform_device grouper_wifi_device = {
+	.name		= "bcmdhd_wlan",
+	.id		= 1,
+	.num_resources	= 1,
+	.resource	= wifi_resource,
+	.dev		= {
+		.platform_data = &grouper_wifi_control,
+	},
+};
 
 static struct resource sdhci_resource0[] = {
 	[0] = {
@@ -73,8 +110,25 @@ static struct resource sdhci_resource3[] = {
 	},
 };
 
+static struct embedded_sdio_data embedded_sdio_data2 = {
+	.cccr   = {
+		.sdio_vsn       = 2,
+		.multi_block    = 1,
+		.low_speed      = 0,
+		.wide_bus       = 0,
+		.high_power     = 1,
+		.high_speed     = 1,
+	},
+	.cis  = {
+		.vendor         = 0x02d0,
+		.device         = 0x4330,
+	},
+};
+
 static struct tegra_sdhci_platform_data tegra_sdhci_platform_data2 = {
 	.mmc_data = {
+		.register_status_notify	= grouper_wifi_status_register,
+		.embedded_sdio = &embedded_sdio_data2,
 		.built_in = 1,
 	},
 	.cd_gpio = -1,
@@ -153,11 +207,82 @@ static struct platform_device tegra_sdhci_device3 = {
 	},
 };
 
+static int grouper_wifi_status_register(
+		void (*callback)(int card_present, void *dev_id),
+		void *dev_id)
+{
+	if (wifi_status_cb)
+		return -EAGAIN;
+	wifi_status_cb = callback;
+	wifi_status_cb_devid = dev_id;
+	return 0;
+}
+static int grouper_wifi_set_carddetect(int val)
+{
+	pr_debug("%s: %d\n", __func__, val);
+	if (wifi_status_cb)
+		wifi_status_cb(val, wifi_status_cb_devid);
+	else
+	pr_warning("%s: Nobody to notify\n", __func__);
+	return 0;
+}
+
+static int grouper_wifi_power(int on)
+{
+	pr_err("Powering %s wifi\n", (on ? "on" : "off"));
+
+	gpio_set_value(GROUPER_WLAN_PWR, on);
+	mdelay(100);
+	gpio_set_value(GROUPER_WLAN_RST, on);
+	mdelay(200);
+
+	return 0;
+}
+
+static int grouper_wifi_reset(int on)
+{
+	pr_err("%s: do nothing\n", __func__);
+	return 0;
+}
+
+static int __init grouper_wifi_init(void)
+{
+	int rc;
+
+	rc = gpio_request(GROUPER_WLAN_PWR, "wlan_power");
+	if (rc)
+		pr_err("WLAN_PWR gpio request failed:%d\n", rc);
+	rc = gpio_request(GROUPER_WLAN_RST, "wlan_rst");
+	if (rc)
+		pr_err("WLAN_RST gpio request failed:%d\n", rc);
+	rc = gpio_request(GROUPER_WLAN_WOW, "bcmsdh_sdmmc");
+	if (rc)
+		pr_err("WLAN_WOW gpio request failed:%d\n", rc);
+
+	tegra_gpio_enable(GROUPER_WLAN_PWR);
+	tegra_gpio_enable(GROUPER_WLAN_RST);
+	tegra_gpio_enable(GROUPER_WLAN_WOW);
+
+	rc = gpio_direction_output(GROUPER_WLAN_PWR, 0);
+	if (rc)
+		pr_err("WLAN_PWR gpio direction configuration failed:%d\n", rc);
+	gpio_direction_output(GROUPER_WLAN_RST, 0);
+	if (rc)
+		pr_err("WLAN_RST gpio direction configuration failed:%d\n", rc);
+	rc = gpio_direction_input(GROUPER_WLAN_WOW);
+	if (rc)
+		pr_err("WLAN_WOW gpio direction configuration failed:%d\n", rc);
+
+	platform_device_register(&grouper_wifi_device);
+	return 0;
+}
+
 int __init grouper_sdhci_init(void)
 {
 	platform_device_register(&tegra_sdhci_device3);
 	platform_device_register(&tegra_sdhci_device2);
 	platform_device_register(&tegra_sdhci_device0);
 
+	grouper_wifi_init();
 	return 0;
 }
