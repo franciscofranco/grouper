fe86d4e6ca49d507d56e2aa5dbd9af99cc68516d
jerryyc_hu <jerryyc_hu@asus.com>
2012-04-05 17:33:30 +0800

    Modify default pinmux configuration of low low battery pin.
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/46832
    Tested-by: JerryYC Hu <jerryyc_hu@asus.com>
    Reviewed-by: JerryYC Hu <jerryyc_hu@asus.com>
    Reviewed-by: Sam hblee <Sam_hblee@asus.com>
    Change-Id: I05a2985447ecced71103baa737766dfa8ff9fafe
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/48074
    Tested-by: Jive Hwang <jive_hwang@asus.com>
    Reviewed-by: Jive Hwang <jive_hwang@asus.com>

--- arch/arm/mach-tegra/board-grouper-pinmux.c
+++ arch/arm/mach-tegra/board-grouper-pinmux.c
@@ -381,7 +381,7 @@ static __initdata struct tegra_pingroup_config grouper_pinmux_common[] = {
 	DEFAULT_PINMUX(SPI2_MISO,       SPI2,            NORMAL,    NORMAL,     INPUT),
 	DEFAULT_PINMUX(SPI2_MOSI,       SPI2,            NORMAL,    NORMAL,     INPUT),
 
-	DEFAULT_PINMUX(KB_ROW12,        KBC,             NORMAL,    TRISTATE,   OUTPUT),
+	DEFAULT_PINMUX(KB_ROW12,        KBC,             NORMAL,    NORMAL,   INPUT),
 	DEFAULT_PINMUX(KB_ROW13,        KBC,             NORMAL,    TRISTATE,   OUTPUT),
 	/*memory bootstrap*/
 	DEFAULT_PINMUX(GMI_AD4,         NAND,           NORMAL,     NORMAL,     INPUT),
