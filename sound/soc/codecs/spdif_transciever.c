7dca0d1c0c8794a6e34103004d06f308a5deffb3
Daniel Solomon <daniels@nvidia.com>
2012-05-02 19:32:22 -0700

    asoc: codecs: spdif: Add grouper support
    Enable grouper support for bias level setting and addition of
    dummy widgets as required by new ALSA driver.
    
    Change-Id: I7263c1b95e7f9d6227dd1366f46cf08e860c652f
    Signed-off-by: Daniel Solomon <daniels@nvidia.com>

--- sound/soc/codecs/spdif_transciever.c
+++ sound/soc/codecs/spdif_transciever.c
@@ -77,7 +77,7 @@ static struct snd_soc_dai_driver dit_stub_dai = {
 
 static int spdif_dit_probe(struct platform_device *pdev)
 {
-	if(machine_is_kai())
+	if(machine_is_kai() || machine_is_grouper())
 		return snd_soc_register_codec(&pdev->dev,
 			&soc_codec_spdif_dit1, &dit_stub_dai, 1);
 	else
