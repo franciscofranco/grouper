bcaef3799c45146b8391999d010bb017a580d6ab
Dima Zavin <dima@android.com>
2012-05-10 09:59:06 -0700

    i2c: tegra: ensure clocks are enabled while in the irq handler
    Change-Id: I94bd5fba06ceaef8e8d9c8aecc26c56f4eabef84
    Signed-off-by: Dima Zavin <dima@android.com>

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -497,6 +497,8 @@ static irqreturn_t tegra_i2c_isr(int irq, void *dev_id)
 	const u32 status_err = I2C_INT_NO_ACK | I2C_INT_ARBITRATION_LOST | I2C_INT_TX_FIFO_OVERFLOW;
 	struct tegra_i2c_dev *i2c_dev = dev_id;
 
+	tegra_i2c_clock_enable(i2c_dev);
+
 	status = i2c_readl(i2c_dev, I2C_INT_STATUS);
 
 	if (status == 0) {
@@ -579,6 +581,7 @@ static irqreturn_t tegra_i2c_isr(int irq, void *dev_id)
 		complete(&i2c_dev->msg_complete);
 	}
 
+	tegra_i2c_clock_disable(i2c_dev);
 	return IRQ_HANDLED;
 
 err:
@@ -616,6 +619,7 @@ err:
 	if (i2c_dev->is_dvc)
 		dvc_writel(i2c_dev, DVC_STATUS_I2C_DONE_INTR, DVC_STATUS);
 
+	tegra_i2c_clock_disable(i2c_dev);
 	complete(&i2c_dev->msg_complete);
 	return IRQ_HANDLED;
 }
