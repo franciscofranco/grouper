e14ed7f4ef6815d3d714e008a32499a7538d30a2
ban_feng <Ban_Feng@asus.com>
2012-05-10 17:17:21 +0800

    Revert "eMMC: Bypass Kingston(MID:0x70) to avoid eMMC can't run at 52MHz"
    Due to update firmware version from 0x19 to 0x26,
    Kingston's eMMC can run at 52MHz.

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -293,12 +293,8 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 	switch (ext_csd[EXT_CSD_CARD_TYPE] & EXT_CSD_CARD_TYPE_MASK) {
 	case EXT_CSD_CARD_TYPE_DDR_52 | EXT_CSD_CARD_TYPE_52 |
 	     EXT_CSD_CARD_TYPE_26:
-		card->ext_csd.hs_max_dtr = 52000000;
-		/* The value of Kingston's Manufacturer ID: 0x70
-		   Bypass 0x70 to avoid eMMC can't run at 52MHz  */
-		if (card->cid.manfid != 0x70) {
-			card->ext_csd.card_type = EXT_CSD_CARD_TYPE_DDR_52;
-		}
+		card->ext_csd.hs_max_dtr = 26000000;
+		card->ext_csd.card_type = EXT_CSD_CARD_TYPE_DDR_52;
 		break;
 	case EXT_CSD_CARD_TYPE_DDR_1_2V | EXT_CSD_CARD_TYPE_52 |
 	     EXT_CSD_CARD_TYPE_26:
