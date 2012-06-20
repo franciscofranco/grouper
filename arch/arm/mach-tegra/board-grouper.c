cc6e6700b6d5bc920bf98a3e3a1fd1d7011d15b7
yi-hsin_hung <yi-hsin_hung@asus.com>
2012-06-20 15:05:09 +0800

    arm: tegra: grouper: enable hotplug and power_down_on_bus_suspend on otg port
    Change-Id: I7e70fe90379679710e6ce6e98c5ead7127b66e0d

--- arch/arm/mach-tegra/board-grouper.c
+++ arch/arm/mach-tegra/board-grouper.c
@@ -748,8 +748,9 @@ static int __init grouper_touch_init(void)
 static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 	[0] = {
 			.phy_config = &utmi_phy_config[0],
-			.operating_mode = TEGRA_USB_HOST,
-			.power_down_on_bus_suspend = 0,
+			.operating_mode = TEGRA_USB_OTG,
+			.power_down_on_bus_suspend = 1,
+			.hotplug = 1,
 			.default_enable = true,
 	},
 	[1] = {
