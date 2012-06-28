e5cb44d30e748d316d58048371f28e3a780dab29
franciscofranco <franciscofranco.1990@gmail.com>
2012-06-28 05:46:48 +0100

    dhd_linux.c: add wifi_pm to toggle betweenPM_FAST and PM_MAX

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -42,6 +42,8 @@
 #include <linux/ethtool.h>
 #include <linux/fcntl.h>
 #include <linux/fs.h>
+#include <linux/moduleparam.h>
+#include <linux/module.h>
 
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
@@ -525,6 +527,9 @@ static void dhd_set_packet_filter(int value, dhd_pub_t *dhd)
 #endif
 }
 
+bool wifi_pm = false;
+module_param(wifi_pm, bool, 0755);
+
 static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 {
 	int power_mode = PM_MAX;
@@ -535,6 +540,11 @@ static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 
 	DHD_TRACE(("%s: enter, value = %d in_suspend=%d\n",
 		__FUNCTION__, value, dhd->in_suspend));
+
+	if (wifi_pm) {
+		power_mode = PM_FAST;
+		pr_info("[franciscofranco] %p Wi-Fi Power Management policy changed to PM_FAST.", __func__);
+	}
 
 	dhd_suspend_lock(dhd);
 	if (dhd && dhd->up) {
