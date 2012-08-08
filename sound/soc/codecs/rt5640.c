9a9baf27c54d8ede2f0ce038f6b1fbd567a27268
sam_chen <sam_chen@asus.com>
2012-08-08 14:30:40 +0800

    ASoc: codecs: rt5640 :fix high-pitched noise issue when headphone insertion during EQ enabled.
    1.it's a codec Realtek RT5640 common issue.
    2.A DC calibration in codec will be performed when headphone plugging.
      If EQ enabled at that moment,
      it will produce high frequency noise at no matter speaker or headphone.
      Therefore, in order to avoid this situation,
      we must disable EQ before DC calibration and then re-enable it afterward.
    
    3.Since we use EQ for speaker playback,
      this issue is most easily reproduced when speaker and headphone playback simultaneously,
      e.g, Notification streams.
    
    Change-Id: I5a513bece745186c5a5890d074ff1486912c96b9
    Signed-off-by: sam_chen <sam_chen@asus.com>

--- sound/soc/codecs/rt5640.c
+++ sound/soc/codecs/rt5640.c
@@ -1620,10 +1620,15 @@ static int rt5640_hp_event(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_codec *codec = w->codec;
+	unsigned int val;
 
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
+		val = snd_soc_read(codec,0xb1);
+		snd_soc_update_bits(codec,0xb1,0x007f,0x0000);
 		rt5640_pmu_depop(codec);
+		snd_soc_write(codec,0xb1,val);
+		snd_soc_write(codec,0xb0,0x6000);
 		break;
 
 	case SND_SOC_DAPM_PRE_PMD:
