cd0fdacc253e664352bde967afc01ce2cb35af3f
singhome_lee <singhome_lee@asus.com>
2012-08-31 15:18:52 +0800

    To erase the registers of TPS6591's RTC alarm to avoid the device power-on by RTC alarm.
    Change-Id: I8253b060d624c1fc0756d908d0e33186152f8aa6
    Reviewed-on: http://mcrd1-5.corpnet.asus/code-review/master/58048
    Reviewed-by: Singhome Lee <singhome_lee@asus.com>
    Tested-by: Singhome Lee <singhome_lee@asus.com>

--- drivers/mfd/tps6591x.c
+++ drivers/mfd/tps6591x.c
@@ -82,6 +82,8 @@
 #define TPS6591X_GPIO_DIR	2
 
 #define GPIO4_F_IT_MSK (1 << 1)
+#define RTC_ALARM_IT_MSK (1 << 6)
+
 enum irq_type {
 	EVENT,
 	GPIO,
@@ -1094,6 +1096,7 @@ static int __devexit tps6591x_i2c_remove(struct i2c_client *client)
 	return 0;
 }
 #ifdef CONFIG_PM
+
 static int tps6591x_i2c_suspend(struct i2c_client *client, pm_message_t state)
 {
 	if (client->irq)
@@ -1116,6 +1119,9 @@ static int tps6591x_i2c_shutdown(struct i2c_client *client)
 	ret = tps6591x_set_bits(&client->dev, TPS6591X_INT_MSK3, GPIO4_F_IT_MSK);
 	if (ret < 0)
 		pr_err("%s(): Setting GPIO4_F_IT_MSK fail.\n", __func__);
+	ret = tps6591x_set_bits(&client->dev, TPS6591X_INT_MSK, RTC_ALARM_IT_MSK);
+	if (ret < 0)
+		pr_err("%s(): Setting RTC_ALARM_IT_MSK fail.\n", __func__);
 	return ret;
 }
 
