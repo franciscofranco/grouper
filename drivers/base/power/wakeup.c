3b2d47bb72a363da1d8961037eb46d6173fc7e92
John Stultz <john.stultz@linaro.org>
2012-09-06 14:07:37 -0400

    wakeup: Use irqsave/irqrestore for events_lock
    Jon Medhurst (Tixy) recently noticed a problem with the
    events_lock usage. One of the Android patches that uses
    wakeup_sources calls wakeup_source_add() with irqs disabled.
    However, the event_lock usage in wakeup_source_add() uses
    spin_lock_irq()/spin_unlock_irq(), which reenables interrupts.
    This results in lockdep warnings.
    
    The fix is to use spin_lock_irqsave()/spin_lock_irqrestore()
    instead for the events_lock.
    
    Full bug report here:
    https://bugs.launchpad.net/linaro-landing-team-arm/+bug/1037565
    
    Cc: Rafael J. Wysocki <rjw@sisk.pl>
    Cc: Android Team <kernel-team@android.com>
    Cc: Arve Hjønnevåg <arve@android.com>
    Cc: Jon Medhurst (Tixy) <tixy@linaro.org>
    Reported-and-debugged-by: Jon Medhurst (Tixy) <tixy@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: Id455188931229f04e353969af389e49b069cb34c

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -101,15 +101,17 @@ EXPORT_SYMBOL_GPL(wakeup_source_destroy);
  */
 void wakeup_source_add(struct wakeup_source *ws)
 {
+	unsigned long flags;
+
 	if (WARN_ON(!ws))
 		return;
 
 	setup_timer(&ws->timer, pm_wakeup_timer_fn, (unsigned long)ws);
 	ws->active = false;
 
-	spin_lock_irq(&events_lock);
+	spin_lock_irqsave(&events_lock, flags);
 	list_add_rcu(&ws->entry, &wakeup_sources);
-	spin_unlock_irq(&events_lock);
+	spin_unlock_irqrestore(&events_lock, flags);
 }
 EXPORT_SYMBOL_GPL(wakeup_source_add);
 
@@ -119,12 +121,14 @@ EXPORT_SYMBOL_GPL(wakeup_source_add);
  */
 void wakeup_source_remove(struct wakeup_source *ws)
 {
+	unsigned long flags;
+
 	if (WARN_ON(!ws))
 		return;
 
-	spin_lock_irq(&events_lock);
+	spin_lock_irqsave(&events_lock, flags);
 	list_del_rcu(&ws->entry);
-	spin_unlock_irq(&events_lock);
+	spin_unlock_irqrestore(&events_lock, flags);
 	synchronize_rcu();
 }
 EXPORT_SYMBOL_GPL(wakeup_source_remove);
@@ -648,17 +652,19 @@ bool pm_get_wakeup_count(unsigned int *count)
 bool pm_save_wakeup_count(unsigned int count)
 {
 	unsigned int cnt, inpr;
+	unsigned long flags;
 
 	events_check_enabled = false;
-	spin_lock_irq(&events_lock);
+	spin_lock_irqsave(&events_lock, flags);
 	split_counters(&cnt, &inpr);
 	if (cnt == count && inpr == 0) {
 		saved_count = count;
 		events_check_enabled = true;
 	}
-	spin_unlock_irq(&events_lock);
+	spin_unlock_irqrestore(&events_lock, flags);
 	if (!events_check_enabled)
 		pm_wakeup_update_hit_counts();
+		
 	return events_check_enabled;
 }
 
