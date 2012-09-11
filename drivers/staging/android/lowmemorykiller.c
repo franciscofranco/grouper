48b316cacb66314743ce56e0f6b996c90d0360b6
franciscofranco <franciscofranco.1990@gmail.com>
2012-09-11 18:34:41 +0100

    lowmemorykiller.c: original patch from cyanogen. According to the commit message memory compaction is never invoked on Android because we avoid swap and don't allocate huge pages. This makes sense so on this patch we call the compaction when a process is killed to avoid memory fragmentation.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -38,6 +38,7 @@
 #include <linux/rcupdate.h>
 #include <linux/profile.h>
 #include <linux/notifier.h>
+#include <linux/compaction.h>
 
 static uint32_t lowmem_debug_level = 2;
 static int lowmem_adj[6] = {
@@ -56,6 +57,8 @@ static int lowmem_minfree[6] = {
 static int lowmem_minfree_size = 4;
 
 static unsigned long lowmem_deathpending_timeout;
+
+extern int compact_nodes();
 
 #define lowmem_print(level, x...)			\
 	do {						\
@@ -156,6 +159,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
+    if (selected)
+        compact_nodes();
 	return rem;
 }
 
