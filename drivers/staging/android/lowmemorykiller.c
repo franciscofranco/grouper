abd39620fee27c69d90a7224b7294ab5713c8fcd
franciscofranco <franciscofranco.1990@gmail.com>
2012-09-11 19:15:39 +0100

    lowmemorykiller: Use asynchronous compaction - from cyanogen

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -160,7 +160,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
     if (selected)
-        compact_nodes();
+        compact_nodes(false);
 	return rem;
 }
 
