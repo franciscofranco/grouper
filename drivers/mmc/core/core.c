4acc227edfb631d377e14911287c1b73682fc9c2
singhome_lee <singhome_lee@asus.com>
2012-09-20 13:54:43 +0800

    mmc: core: new discard feature support at Samsung eMMC v4.41+.
    Support discard feature if MID field in the CID register is 0x15, EXT.CSD[192]
    (device version) is 5 and Bit 0 in the EXT.CSD[64] is 1. Also removed REQ_SECURE flag
    check to avoid kernel hang.
    
    This patch is released from samsung.
    
    Change-Id: I4023a900680e9bca10c40311b09ed077a22617db

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -1482,7 +1482,10 @@ static unsigned int mmc_mmc_erase_timeout(struct mmc_card *card,
 {
 	unsigned int erase_timeout;
 
-	if (card->ext_csd.erase_group_def & 1) {
+	if (arg == MMC_DISCARD_ARG ||
+		(arg == MMC_TRIM_ARG && card->ext_csd.rev >= 6)) {
+		erase_timeout = card->ext_csd.trim_timeout;
+	} else if (card->ext_csd.erase_group_def & 1) {
 		/* High Capacity Erase Group Size uses HC timeouts */
 		if (arg == MMC_TRIM_ARG)
 			erase_timeout = card->ext_csd.trim_timeout;
@@ -1757,6 +1760,14 @@ int mmc_can_trim(struct mmc_card *card)
 	return 0;
 }
 EXPORT_SYMBOL(mmc_can_trim);
+
+int mmc_can_discard(struct mmc_card *card)
+{
+	if (card->ext_csd.feature_support & MMC_DISCARD_FEATURE)
+		return 1;
+	return 0;
+}
+EXPORT_SYMBOL( mmc_can_discard);
 
 int mmc_can_secure_erase_trim(struct mmc_card *card)
 {
--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -439,6 +439,9 @@ static int mmc_read_ext_csd(struct mmc_card *card, u8 *ext_csd)
 	else
 		card->erased_byte = 0x0;
 
+	if ((card->cid.manfid == 0x15) && (ext_csd[64] & 0x01))
+		card->ext_csd.feature_support |= MMC_DISCARD_FEATURE;
+
 out:
 	return err;
 }
