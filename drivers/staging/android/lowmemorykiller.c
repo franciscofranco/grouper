4b10f5fb4c41077c53f0300b699a7ee0f09e6608
franciscofranco <franciscofranco.1990@gmail.com>
2012-10-08 04:29:20 +0100

    Revert "lowmemorykiller: Use asynchronous compaction - from cyanogen"
    This reverts commit abd39620fee27c69d90a7224b7294ab5713c8fcd.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -161,7 +161,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
     if (selected)
-        compact_nodes(false);
+        compact_nodes();
 	return rem;
 }
 
