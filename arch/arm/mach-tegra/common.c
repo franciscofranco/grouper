37a9bd32d22407dfaf689132c00abad9457adb1d
franciscofranco <franciscofranco.1990@gmail.com>
2012-10-12 04:12:16 +0100

    tegra: decrease GPU clocks to nearly stock values. Also refactor most code to make it simpler. We get rid of the voltages arrays and update the millivolts table dynamically. For that we had to get rid of the const variable type or the compiler wouldn't let it build for obvious reasons. Also changes how to change the frequency of the GPU as it only outputs one value instead of the whole array of frequencies. Makes it much easier for users to use and much easy on the code complexity. Thanks @morfic for the pll_c tables.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -103,7 +103,7 @@ static struct board_info pmu_board_info;
 static struct board_info display_board_info;
 static struct board_info camera_board_info;
 
-static int pmu_core_edp = 1500;	/* default 1.2V EDP limit */
+static int pmu_core_edp = 1550;	/* default 1.2V EDP limit */
 static int board_panel_type;
 static enum power_supply_type pow_supply_type = POWER_SUPPLY_TYPE_MAINS;
 
