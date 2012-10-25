c8e1f6c79b3c0eb5e928e57aef85fb21935eab4b
Tsechih_Lin <Tsechih_Lin@asus.com>
2012-10-25 23:55:23 +0800

    Fix al3010 power on fail in late_resume function
    When interval time is too short between al3101_early_suspend() and al3010_late_resume().
    It will cause driver fail to set power on. So , add 5ms delay to fix this issue.
    
    Change-Id: I2523b41384b47cbb23100fdf7f451f54bdeaed19

--- drivers/hwmon/al3010.c
+++ drivers/hwmon/al3010.c
@@ -455,6 +455,9 @@ static int al3010_late_resume(struct early_suspend *h)
 	int ret=0;
 	//+++
 	struct al3010_data *data = container_of(h, struct al3010_data, light_sensor_early_suspender);
+    //delay 5ms to avoid al3010_early_suspend and al3010_late_resume too close.
+    //if too close , it would cause al3010 chip power on fail
+    mdelay(5);
 	ret = al3010_chip_resume(data);
 	//---
 	printk("al3010_late_resume-\n");
