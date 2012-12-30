643ae8322b746d35211cfcd146d64ae08ab1002e
franciscofranco <franciscofranco.1990@gmail.com>
2012-12-30 13:44:08 -0800

    arch: arm: tegra3: make cpu_g cluster to use all the frequency table.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-tegra/tegra3_clocks.c
+++ arch/arm/mach-tegra/tegra3_clocks.c
@@ -3909,6 +3909,7 @@ static struct clk tegra_clk_virtual_cpu_g = {
 	.parent    = &tegra_clk_cclk_g,
 	.ops       = &tegra_cpu_ops,
 	.max_rate  = 1700000000,
+	.min_rate  = 100000000,
 	.u.cpu = {
 		.main      = &tegra_pll_x,
 		.backup    = &tegra_pll_p,
@@ -4689,8 +4690,8 @@ static int clip_cpu_rate_limits(
 		       cpu_clk_lp->max_rate, ret ? "outside" : "at the bottom");
 		return ret;
 	}
-	cpu_clk_lp->max_rate = freq_table[idx].frequency * 1000;
-	cpu_clk_g->min_rate = freq_table[idx-1].frequency * 1000;
+	//cpu_clk_lp->max_rate = freq_table[idx].frequency * 1000;
+	cpu_clk_g->min_rate = 100000;
 	data->suspend_index = idx;
 	return 0;
 }
