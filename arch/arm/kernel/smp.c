233fc154d23f94cb601906349ed17910542c900a
Francisco Franco <franciscofranco.1990@gmail.com>
2013-04-07 11:59:20 -0700

    smp: we already have debug pr_info from tegra_hotplug, no need to get some debug from here.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -215,7 +215,7 @@ void __cpu_die(unsigned int cpu)
 		pr_err("CPU%u: cpu didn't die\n", cpu);
 		return;
 	}
-	printk(KERN_NOTICE "CPU%u: shutdown\n", cpu);
+	//printk(KERN_NOTICE "CPU%u: shutdown\n", cpu);
 
 	if (!platform_cpu_kill(cpu))
 		printk("CPU%u: unable to kill\n", cpu);
@@ -280,7 +280,7 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	struct mm_struct *mm = &init_mm;
 	unsigned int cpu = smp_processor_id();
 
-	printk("CPU%u: Booted secondary processor\n", cpu);
+	//printk("CPU%u: Booted secondary processor\n", cpu);
 
 	/*
 	 * All kernel threads share the same mm context; grab a
