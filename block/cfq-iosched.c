7cea124820efcab71bbb1b4dd74e10bc946db2ae
Francisco Franco <franciscofranco.1990@gmail.com>
2013-04-21 23:56:39 -0700

    block: cfq: tweak all CFQ tunables. Researched by my mates malaroth, osm0sis, joaquinf, The Gingerbread Man, pkgnex, Khrushy, shreddintyres. Please if you copy this values make sure you cherry-pick it properly and leave these guys in the commit message, they researched this for a long time.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -20,18 +20,18 @@
  * tunables
  */
 /* max queue in one round of service */
-static const int cfq_quantum = 8;
-static const int cfq_fifo_expire[2] = { HZ / 4, HZ / 8 };
+static const int cfq_quantum = 4;
+static const int cfq_fifo_expire[2] = { 80, 330};
 /* maximum backwards seek, in KiB */
-static const int cfq_back_max = 16 * 1024;
+static const int cfq_back_max = 12582912;
 /* penalty of a backwards seek */
-static const int cfq_back_penalty = 2;
-static const int cfq_slice_sync = HZ / 10;
-static int cfq_slice_async = HZ / 25;
+static const int cfq_back_penalty = 1;
+static const int cfq_slice_sync = 60;
+static int cfq_slice_async = 50;
 static const int cfq_slice_async_rq = 2;
-static int cfq_slice_idle = HZ / 125;
-static int cfq_group_idle = HZ / 125;
-static const int cfq_target_latency = HZ * 3/10; /* 300 ms */
+static int cfq_slice_idle = 0;
+static int cfq_group_idle = 0;
+static const int cfq_target_latency = 300; /* 300 ms */
 static const int cfq_hist_divisor = 4;
 
 /*
@@ -4268,20 +4268,6 @@ static struct blkio_policy_type blkio_policy_cfq;
 
 static int __init cfq_init(void)
 {
-	/*
-	 * could be 0 on HZ < 1000 setups
-	 */
-	if (!cfq_slice_async)
-		cfq_slice_async = 1;
-	if (!cfq_slice_idle)
-		cfq_slice_idle = 1;
-
-#ifdef CONFIG_CFQ_GROUP_IOSCHED
-	if (!cfq_group_idle)
-		cfq_group_idle = 1;
-#else
-		cfq_group_idle = 0;
-#endif
 	if (cfq_slab_setup())
 		return -ENOMEM;
 
