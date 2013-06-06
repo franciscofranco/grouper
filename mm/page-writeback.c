d17e66912e99201a72def83ed7123cda1f442300
Francisco Franco <franciscofranco.1990@gmail.com>
2013-06-06 06:01:23 +0100

    writeback: remove early_suspend setting as it could cause data loss on loss of power.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -100,7 +100,7 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 0; /* centiseconds */
+unsigned int dirty_writeback_interval = 15 * 100; /* centiseconds */
 
 /*
  * The longest time for which data is allowed to remain dirty
@@ -968,21 +968,6 @@ static struct notifier_block __cpuinitdata ratelimit_nb = {
 	.next		= NULL,
 };
 
-static void dirty_early_suspend(struct early_suspend *handler)
-{
-	dirty_writeback_interval = 5 * 100;
-}
-
-static void dirty_late_resume(struct early_suspend *handler)
-{
-	dirty_writeback_interval = 0;
-}
-
-static struct early_suspend dirty_suspend = {
-	.suspend = dirty_early_suspend,
-	.resume = dirty_late_resume,
-};
-
 /*
  * Called early on to tune the page writeback dirty limits.
  *
@@ -1005,8 +990,6 @@ void __init page_writeback_init(void)
 {
 	int shift;
 	
-	register_early_suspend(&dirty_suspend);
-
 	writeback_set_ratelimit();
 	register_cpu_notifier(&ratelimit_nb);
 
