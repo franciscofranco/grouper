caaa5a6b5187ced8eef0fa3cf463ec4fb73322f3
Francisco Franco <franciscofranco.1990@gmail.com>
2013-11-12 19:14:51 +0000

    suspend: remove disable/enable cpus every time suspend is called (which is called every damn 5 seconds -.-).
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -162,10 +162,6 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	if (suspend_test(TEST_PLATFORM))
 		goto Platform_wake;
 
-	error = disable_nonboot_cpus();
-	if (error || suspend_test(TEST_CPUS))
-		goto Enable_cpus;
-
 	arch_suspend_disable_irqs();
 	BUG_ON(!irqs_disabled());
 
@@ -181,9 +177,6 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
-
- Enable_cpus:
-	enable_nonboot_cpus();
 
  Platform_wake:
 	if (suspend_ops->wake)
