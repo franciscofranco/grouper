d1575c0c7fd7e9d83d10e074c9afc88d5e452312
franciscofranco <franciscofranco.1990@gmail.com>
2015-01-19 17:37:21 +0000

    Revert "[PATCH, v9] cgroups: introduce timer slack controller"
    This reverts commit ec5291e409af821d07b1d89cac3fe8df3433374c.

--- fs/select.c
+++ fs/select.c
@@ -69,6 +69,7 @@ static long __estimate_accuracy(struct timespec *tv)
 
 long select_estimate_accuracy(struct timespec *tv)
 {
+	unsigned long ret;
 	struct timespec now;
 
 	/*
@@ -80,8 +81,10 @@ long select_estimate_accuracy(struct timespec *tv)
 
 	ktime_get_ts(&now);
 	now = timespec_sub(*tv, now);
-	return min_t(long, __estimate_accuracy(&now),
-			task_get_effective_timer_slack(current));
+	ret = __estimate_accuracy(&now);
+	if (ret < current->timer_slack_ns)
+		return current->timer_slack_ns;
+	return ret;
 }
 
 
